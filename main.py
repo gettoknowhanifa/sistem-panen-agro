d572b7e5fc23c81a72770e5a9c842cf8dbb33581
Dmssptdi05 <dmssptdi05@gmail.com>
2026-09-18 04:53:54 +0000

    Menambahkan fungsi cetak laporan oleh Dimas

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Sistem Pencatatan Hasil Panen Agroindustri
 # Terdiri dari fungsi input, perhitungan, dan laporan.
+
+def cetak_laporan(komoditas, berat, total_pendapatan):
+    print("\n" + "="*40)
+    print("      STRUK HASIL PANEN AGRO")
+    print("="*40)
+    print(f"Komoditas     : {komoditas}")
+    print(f"Berat         : {berat} kg")
+    print(f"Total Estimasi: Rp {total_pendapatan:,.0f}")
+    print("="*40)
