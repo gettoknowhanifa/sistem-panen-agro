d7087b3b0cb4fee77dfe15cfbf3b5a69b90e601c
sifaoctavia-hub <sifanuroctaviani@gmail.com>
2026-09-18 04:33:35 +0000

    Menambahkan fungsi hitung pendapatan oleh Sifa

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 # Sistem Pencatatan Hasil Panen Agroindustri
 # Terdiri dari fungsi input, perhitungan, dan laporan.
+
+def hitung_pendapatan(berat_kg, harga_per_kg=5000):
+    total = berat_kg * harga_per_kg
+    return total
