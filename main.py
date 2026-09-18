3fbc1135a87b9a202902edf79ad0571a94c8b8ae
gettoknowhanifa <gettoknowhanifa@apps.ipb.ac.id>
2026-09-18 05:30:59 +0000

    Menambahkan fungsi input data panen

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # Sistem Pencatatan Hasil Panen Agroindustri
 # Terdiri dari fungsi input, perhitungan, dan laporan.
+
+def input_data_panen():
+    komoditas = input("Masukkan nama komoditas (contoh: Kopi Arabika): ")
+    berat = float(input("Masukkan berat panen (kg): "))
+    return komoditas, berat
